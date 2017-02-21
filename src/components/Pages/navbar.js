c6039aa5b06757638ba672ab30dd150240b8bbf6
Todor Krastev <to6esko69@gmail.com>
2017-02-21 08:29:04 +0200

    css max-width 320

--- src/components/Pages/navbar.js
+++ src/components/Pages/navbar.js
@@ -18,24 +18,26 @@ export default class Navbar extends React.Component {
     render() {
         return (
             <div>
-                <div className="navbar">
-                    <ul>
-                        <li><Link to="#/home">Начало</Link></li> 
-                        <li className="dropdown"><Link>Услуги</Link>
-                        <ul className="dropdown-content">    
-                            <li><Link to="/computerAndMobile">Сервиз на компютри и мобилни устройства</Link></li>
-                            <li><Link to="/copyingServices">Копирни услуги</Link></li>
-                            <li><Link to="/multimediaServices">Мултимедийни услуги</Link></li>
-                            </ul>    
-                        </li>
-                        <li><Link to="/virtualReality">Виртуална реалност</Link></li>
-                        <li><Link to="/abonament">Абонаментна програма</Link></li>
-                        <li><Link to="/shop">Магазин</Link></li>
-                        <li><Link to="/promo">Промоции</Link></li>
-                        <li><Link to="/contacts">Контакти</Link></li>
+                <nav className="navbar" role="navigation">
+                    <ul className="home">
+                        <li><Link to="#/home" aria-haspopup="true">Начало</Link></li>
+                        <ul className="home-content">
+                            <li className="dropdown"><Link>Услуги</Link>
+                                <ul className="dropdown-content">
+                                    <li><Link to="/computerAndMobile" aria-haspopup="true">Сервиз на компютри и мобилни устройства</Link></li>
+                                    <li><Link to="/copyingServices" aria-haspopup="true">Копирни услуги</Link></li>
+                                    <li><Link to="/multimediaServices" aria-haspopup="true">Мултимедийни услуги</Link></li>
+                                </ul>
+                            </li>
+                            <li><Link to="/virtualReality" aria-haspopup="true">Виртуална реалност</Link></li>
+                            <li><Link to="/abonament" aria-haspopup="true">Абонаментна програма</Link></li>
+                            <li><Link to="/shop" aria-haspopup="true">Магазин</Link></li>
+                            <li><Link to="/promo" aria-haspopup="true">Промоции</Link></li>
+                            <li><Link to="/contacts" aria-haspopup="true">Контакти</Link></li>
+                        </ul>
                     </ul>
-                </div>
-            </div>    
+                </nav>
+            </div>
         )
     }
 }
